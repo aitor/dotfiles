62c5e00387f06761b8253c71713c060a7ae0ea02
Aitor García <aitor.garcia@gmail.com>
2011-10-17 20:48:13 +0000

    Pull your heroku app database via dump. Much faster for non trivial dbs than db:pull

---
+++ ruby/heroku.zsh
@@ -0,0 +1,10 @@
+# Because you know, db:pull is fucking slow...
+function backup:pull () {
+  name=$(heroku info --raw | grep "^name=" | cut -d= -f2)
+  echo "Loading '$name' app's data in local database."
+
+  heroku pgbackups:capture --expire
+  curl -o latest.dump `heroku pgbackups:url`
+  pg_restore --verbose --clean --no-acl --no-owner -d $name latest.dump
+  rm -fr latest.dump
+}
