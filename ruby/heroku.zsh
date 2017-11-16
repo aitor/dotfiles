e9308fb3c60be95b6550d08532810c34a918503c
Aitor García Rey <aitor@linkingpaths.com>
2017-11-16 13:54:50 +0100

    Heroku backup scripts updated.

--- ruby/heroku.zsh
+++ ruby/heroku.zsh
@@ -1,10 +1,36 @@
 # Because you know, db:pull is fucking slow...
 function backup:pull () {
-  name=$(heroku info --shell | grep "^name=" | cut -d= -f2)
-  echo "Loading '$name' app's data in local database."
+  force=false
+  set -- `getopt f: "$@"`
+  [ $# -lt 1 ] && exit 1	# getopt failed
 
-  heroku pgbackups:capture --expire
-  curl -o latest.dump `heroku pgbackups:url`
-  pg_restore --verbose --clean --no-acl --no-owner -d $name latest.dump
+  while [ $# -gt 0 ]
+  do
+      case "$1" in
+          -f)	force=true;;
+  	      --)	shift; break;;
+  	       *)	break;;	# terminate while loop
+      esac
+      shift
+  done
+  # all command line switches are processed,
+  app=$1
+
+  echo "### Loading '$app' app data in local database." >&2
+
+  if [ "$force" = true ] ; then
+    echo "  Capturing a fresh backup…" >&2
+    heroku pg:backups capture -a $app
+  else
+    echo '  Using last existing backup.' >&2
+  fi
+  echo "  Retrieving backup…" >&2
+  echo "  " >&2
+  wget `heroku pg:backups public-url -a $app` -O latest.dump --continue
+  echo "  " >&2
+  echo "  Backup downloaded. Restoring in local DB…" >&2
+  pg_restore --verbose --clean --no-acl --no-owner -d $app latest.dump
+  echo "  Cleaning up…" >&2
   rm -fr latest.dump
+  echo "Data loaded." >&2
 }
